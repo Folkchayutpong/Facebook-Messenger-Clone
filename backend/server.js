8e6a3bfcd84846012d35e42e41c7d2b3ce830ef5
Folkchayutpong <folkchayutpong@gmail.com>
2025-07-04 15:53:40 +0700

    feat: conect to db and use env

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,28 @@
+const path = require("path");
 const app = require("./app");
 const http = require("http");
+require("dotenv").config({ path: "./config/.env" });
+const mongoose = require("mongoose");
 
+const port = process.env.PORT || 3000;
+
+//create server
 const server = http.createServer(app);
 
+// conect to mongodb
+try {
+  mongoose.connect("mongodb://localhost:27017/mydb");
+  console.log("Connected to MongoDB");
+} catch (e) {
+  console.log(e);
+}
+
+//test route
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-server.listen(5000, () => {
-  console.log("Server is running on port 5000");
+//start server
+server.listen(port, async () => {
+  console.log(`Server is running on port ${port}`);
 });
