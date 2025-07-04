c914629f8b8488f7a21e2854e499f109dd7a294a
Folkchayutpong <folkchayutpong@gmail.com>
2025-07-05 01:42:22 +0700

    refactor: refactor code

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,11 +2,14 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 // conect to mongodb
-try {
-  mongoose.connect(process.env.MONGODB_URL);
-  console.log("Connected to MongoDB");
-} catch (e) {
-  console.error("MongoDB connection error:", err);
-}
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL);
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error(" MongoDB connection error:", err);
+    process.exit(1);
+  }
+};
 
-module.exports = mongoose;
+module.exports = connectDB;
