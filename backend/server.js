675e5b7a86a75cf4d9a91c5f3bd1a9609c2196c4
Folkchayutpong <folkchayutpong@gmail.com>
2025-07-05 01:48:17 +0700

    fix: add / at front of api route

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,8 @@
-const path = require("path");
 const app = require("./app");
 const http = require("http");
 const userRoute = require("./modules/user/user.route");
+const conectDB = require("./config/db");
 require("dotenv").config({ path: "./config/.env" });
-require("./config/db");
 
 const port = process.env.PORT || 3000;
 
@@ -11,18 +10,12 @@ const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 
 //route
-app.use("api/user", userRoute);
+app.use("/api/user", userRoute);
 
 //TODO: add other routes
 
-
-
-
-
-
-
-
 //start server
 server.listen(port, async () => {
+  await conectDB();
   console.log(`Server is running on port ${port}`);
 });
