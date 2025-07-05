5250329ed93c7416cffe84e173ae3ae589a4dcea
Folkchayutpong <folkchayutpong@gmail.com>
2025-07-06 00:46:42 +0700

    feat: add socket setup function

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const { Server } = require("socket.io");
+const socketAuthMiddleware = require("./middleware/auth");
 
 //setup express
 const app = express();
@@ -14,4 +16,20 @@ app.use(express.urlencoded());
 app.use(express.json());
 app.use(cors(corsOptions));
 
-module.exports = app;
+function initSocket(server) {
+  const io = new Server(server, {
+    cors: corsOptions,
+  });
+
+  io.use(socketAuthMiddleware);
+
+  io.on("connection", (socket) => {
+    console.log(`Socket connected: ${socket.user.email}`);
+
+    socket.on("disconnect", () => {
+      console.log(` Socket disconnected: ${socket.user.email}`);
+    });
+  });
+}
+
+module.exports = { app, initSocket };
