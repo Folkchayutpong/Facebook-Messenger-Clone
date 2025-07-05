ff2d218eae50cfbdfb2eb54781ae32c57540cd6f
Folkchayutpong <folkchayutpong@gmail.com>
2025-07-06 01:34:53 +0700

    feat: add parseTokenFromCookie

--- backend/utils/utils.js
+++ backend/utils/utils.js
@@ -1,13 +1,19 @@
 const jwt = require("jsonwebtoken");
+const cookie = require("cookie");
 require("dotenv").config({ path: "./config/.env" });
-
 //generate access token
 function generateAccessToken(id, email) {
-  return jwt.sign({ id: id, email: email }, process.env.JWT_TOKEN, {
+  return jwt.sign({ id: id, email: email }, process.env.JWT_SECRET, {
     expiresIn: "1d",
   });
 }
 
+const parseTokenFromCookie = (cookieString) => {
+  const cookies = cookie.parse(cookieString);
+  return cookies.token;
+};
+
 module.exports = {
   generateAccessToken,
+  parseTokenFromCookie,
 };
