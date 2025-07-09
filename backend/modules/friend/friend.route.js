e1fedd8ebdd552ce802bfd4bf315be64dbac0850
Peerapol Pocharoen <6510615252@cn330>
2025-07-09 18:20:01 +0700

    feat: add authMiddleWare on route

--- backend/modules/friend/friend.route.js
+++ backend/modules/friend/friend.route.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const friendController = require("./friend.controller");
+const  {expressAuthMiddleware}  = require("../../middleware/auth");
 
-router.post("/add", friendController.add);
-router.post("/accept", friendController.accept);
-router.post("/decline", friendController.decline);
-router.post("/remove", friendController.remove);
+router.post("/add",  expressAuthMiddleware , friendController.add);
+router.post("/accept",  expressAuthMiddleware , friendController.accept);
+router.post("/decline",  expressAuthMiddleware , friendController.decline);
+router.post("/remove",  expressAuthMiddleware , friendController.remove);
 
 module.exports = router;
