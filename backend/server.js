28b8e0c3092def34a30fd52ea997dd4aacc7b83d
Peerapol Pocharoen <6510615252@cn330>
2025-07-15 21:37:46 +0700

    feat: chatPage

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const http = require("http");
 const userRoute = require("./modules/user/user.route");
 const friendRoute = require("./modules/friend/friend.route");
 const chatRoute = require("./modules/chat/chat.route");
+const messageRoute = require("./modules/message/message.route");
 const conectDB = require("./config/db");
 const { connectRedis } = require("./config/redis");
 
@@ -16,6 +17,7 @@ const server = http.createServer(app);
 app.use("/api/user", userRoute);
 app.use("/api/friend", friendRoute);
 app.use("/api/chats", chatRoute);
+app.use("/api/messages", messageRoute);
 
 //TODO: add other routes
 
