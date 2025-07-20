4d12907c1fe148c32773f8a9c29b84f0639aeefd
Folkchayutpong <folkchayutpong@gmail.com>
2025-07-20 16:35:23 +0700

    fix: fix avartar bug

--- frontend/src/components/config_chat.jsx
+++ frontend/src/components/config_chat.jsx
@@ -1,5 +1,12 @@
+import Avartar from "./avartar";
 const ConfigChat = () => {
-  return <div className="w-1/4">ConfigChat</div>;
+  return (
+    <div className="w-1/4 bg-base-200 h-screen flex justify-center">
+      <div className="m-15">
+        <Avartar width={30} />
+      </div>
+    </div>
+  );
 };
 
 export default ConfigChat;
