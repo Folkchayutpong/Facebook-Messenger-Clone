50c80c5008c56b597d743aec13fc10d307706443
Folkchayutpong <folkchayutpong@gmail.com>
2025-07-20 23:42:40 +0700

    feat:add setting

--- frontend/src/components/config_chat.jsx
+++ frontend/src/components/config_chat.jsx
@@ -1,9 +1,19 @@
-import Avartar from "./avartar";
+import Avatar from "./avartar";
+
 const ConfigChat = () => {
   return (
-    <div className="w-1/4 bg-base-200 h-screen flex justify-center">
-      <div className="m-15">
-        <Avartar width={30} />
+    <div className="w-1/4 bg-base-300 h-screen flex justify-center p-10">
+      <div className="flex flex-col items-center space-y-5">
+        <Avatar width={30} />
+
+        <span className="font-bold text-2xl">Username</span>
+
+        <div className="flex flex-col items-center space-y-2">
+          <span className="font-semibold text-xl">Setting</span>
+          <span>Config Chat</span>
+          <span>Pictures</span>
+          <span>Files</span>
+        </div>
       </div>
     </div>
   );
